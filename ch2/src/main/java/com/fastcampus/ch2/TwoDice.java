c446eb8bfc49fcc8db126da0773cd95eab2a3183
Yuki <skool109@gmail.com>
2022-11-01 16:21:28 +0900

    feat: Update TwoDice.java

--- ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
+++ ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
@@ -3,14 +3,29 @@ package com.fastcampus.ch2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 public class TwoDice {
 
     @RequestMapping("/rollDice")
-    public void main(HttpServletRequest request, HttpServletResponse response) {
+    public void main(HttpServletResponse response) throws IOException {
+        int idx1 = (int)(Math.random() * 6) + 1;
+        int idx2 = (int)(Math.random() * 6) + 1;
 
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<img src='resources/img/dice" + idx1 + ".jpg'>");
+        out.println("<img src='resources/img/dice" + idx2 + ".jpg'>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
