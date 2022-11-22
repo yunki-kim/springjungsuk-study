1e94f9c5eb5d738c509fbd76a8a89ad72410a42d
Yuki <skool109@gmail.com>
2022-11-22 16:05:45 +0900

    feat: Update MyBatis & DAO example

---
+++ ch4/src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -0,0 +1,26 @@
+package com.fastcampus.ch4.dao;
+
+import com.fastcampus.ch4.domain.BoardDto;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BoardDao {
+    int insert(BoardDto dto) throws Exception;
+
+    BoardDto select(Integer bno) throws Exception;
+
+    List<BoardDto> selectAll() throws Exception;
+
+    List<BoardDto> selectPage(Map map) throws Exception;
+
+    int update(BoardDto dto) throws Exception;
+
+    int delete(Integer bno, String writer) throws Exception;
+
+    int deleteAll();
+
+    int count() throws Exception;
+
+    int increaseViewCnt(Integer bno) throws Exception;
+}
---
+++ ch4/src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
@@ -0,0 +1,65 @@
+package com.fastcampus.ch4.dao;
+
+import com.fastcampus.ch4.domain.BoardDto;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class BoardDaoImpl implements BoardDao {
+
+    @Autowired private SqlSession session;
+    private static String namespace = "com.fastcampus.ch4.dao.BoardMapper.";
+
+    @Override
+    public int insert(BoardDto dto) throws Exception {
+        return session.insert(namespace + "insert", dto);
+    }
+
+    @Override
+    public BoardDto select(Integer bno) throws Exception {
+        return session.selectOne(namespace + "select", bno);
+    }
+
+    @Override
+    public List<BoardDto> selectAll() throws Exception {
+        return session.selectList(namespace + "selectAll");
+    }
+
+    @Override
+    public List<BoardDto> selectPage(Map map) throws Exception {
+        return session.selectList(namespace + "selectPage", map);
+    }
+
+    @Override
+    public int update(BoardDto dto) throws Exception {
+        return session.update(namespace + "update", dto);
+    }
+
+    @Override
+    public int delete(Integer bno, String writer) throws Exception {
+        Map map = new HashMap();
+        map.put("bno", bno);
+        map.put("writer", writer);
+        return session.delete(namespace + "delete", map);
+    }
+
+    @Override
+    public int deleteAll() {
+        return session.delete(namespace + "deleteAll");
+    }
+
+    @Override
+    public int count() throws Exception {
+        return session.selectOne(namespace + "count");
+    }
+
+    @Override
+    public int increaseViewCnt(Integer bno) throws Exception {
+        return session.update(namespace + "increaseViewCnt", bno);
+    }
+}
