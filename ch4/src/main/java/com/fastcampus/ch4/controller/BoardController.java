3e8726b286d0526355e9a43b69a8402b1b5b2810
Yuki <skool109@gmail.com>
2022-11-22 17:24:27 +0900

    feat: Update Paging example

---
+++ ch4/src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -0,0 +1,52 @@
+package com.fastcampus.ch4.controller;
+
+import com.fastcampus.ch4.domain.BoardDto;
+import com.fastcampus.ch4.domain.PageHandler;
+import com.fastcampus.ch4.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired BoardService boardService;
+
+    @GetMapping("/list")
+    public String list(Integer page, Integer pageSize, Model model, HttpServletRequest request) {
+        if(!loginCheck(request)) return "redirect:/login/login?toURL=" + request.getRequestURL();
+
+        if (page == null) page = 1;
+        if (pageSize == null) pageSize = 10;
+
+        try {
+            int totalCnt = boardService.getCount();
+            PageHandler pageHandler = new PageHandler(totalCnt, page, pageSize);
+            Map map = new HashMap();
+            map.put("offset", (page - 1) * pageSize);
+            map.put("pageSize", pageSize);
+
+            List<BoardDto> list = boardService.getPage(map);
+            model.addAttribute("list", list);
+            model.addAttribute("ph", pageHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "boardList";
+    }
+
+    private boolean loginCheck(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        return session.getAttribute("id")!=null;
+    }
+}
